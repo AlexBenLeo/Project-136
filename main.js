7c78ab881fec9c327aa7fd02b5c8e5dd4c15c6f3
AlexBenLeo <85003460+AlexBenLeo@users.noreply.github.com>
2022-03-23 21:40:02 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ video="";
 status="";
 objects=[];
 function setup() {
-    canvas=createCanvas(480,380);
+    canvas=createCanvas(400,250);
     canvas.center();
     video=createCapture(VIDEO);
-    video.size(480,380);
+    video.size(400,250);
     video.hide();
 }
 function draw() {
-    image(video,0,0,480,380);
+    image(video,0,0,400,250);
     if(status!=""){
+        objectDetector.detect(video,gotResult);
         for(i=0; i<objects.length; i++) {
             fill("#FF0000");
  percent=floor(objects[i].confidence*100);
@@ -18,6 +19,17 @@ text(objects[i].label+""+percent+"%",objects[i].x+15,objects[i].y+15);
 noFill();
 stroke("#FF0000");
 rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+if(object[i].label==object_name) {
+    video.stop();
+    objectDetector.detect(gotResult);
+    document.getElementById("object_status").innerHTML=object_name+"found";
+    synth=window.speechSynthesis;
+    utterThis=new SpeechSynthesisUtterance(object_name+"found");
+    synth.speak(utterThis);
+}
+else{
+    document.getElementById("object_status").innerHTML=object_name+"not found";
+}
 
         }
 
